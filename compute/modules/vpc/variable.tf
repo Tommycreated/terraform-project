584b5afe48d407ea4609e69dbf585fbf57b8206e
philip tom <124618639+Tommycreated@users.noreply.github.com>
2024-04-27 09:57:09 +0100

    Add files via upload

---
+++ compute/modules/vpc/variable.tf
@@ -0,0 +1,22 @@
+variable "vpc_cidr" {
+  type = string
+}
+variable "vpc_tag" {
+  type = string
+}
+variable "sub1_cidr_block" {
+  type = string
+}
+variable "sub1_availability_zone" {
+  type = string
+}
+variable "sub2_cidr_block" {
+  type = string
+}
+variable "sub2_availability_zone" {
+  type = string
+}
+
+variable "map_public_ip_on_launch" {
+  type = string
+}
